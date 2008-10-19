1b9c7bfb7d6bbb3a832c43d1dc972c0d5da52b04
Klemens Ullmann <klemens@ull.at>
2008-10-19 17:42:44 +0000

    added symfony's lib & data directorys as svn:external initialized empty symfony project generated "frontend" app
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineActAsTaggablePlugin/branches/1.1@12254 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ dev_environment/config/ProjectConfiguration.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+  }
+}
