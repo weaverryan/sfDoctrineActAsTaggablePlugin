44a8e955dfe136eff86a83ebde8db2d062aa091e
Mickael Kurmann <mickael.kurmann@gmail.com>
2008-10-26 19:26:01 +0000

    path update
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineActAsTaggablePlugin/branches/1.1@12371 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- dev_environment/config/ProjectConfiguration.class.php
+++ dev_environment/config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__) . '/../vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
