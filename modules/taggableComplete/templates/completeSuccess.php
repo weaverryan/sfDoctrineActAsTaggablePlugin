254474eda8fc83787796236744b4f293ba3fc74f
John Benson <johnny@punkave.com>
2009-07-13 20:55:51 +0000

    changed some markup
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineActAsTaggablePlugin/branches/punkave@20167 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/taggableComplete/templates/completeSuccess.php
+++ modules/taggableComplete/templates/completeSuccess.php
@@ -1,6 +1,6 @@
 <ul>
 <?php foreach($tagSuggestions as $suggestion): ?>
 <?php // No extraneous whitespace here, it shows up in text() ?>
-<li><?php echo $suggestion['left']?><a href="#"><?php echo $suggestion['suggested']?></a><?php echo $suggestion['right']?></li>
+<li><span class="tag-spacer left"><?php echo $suggestion['left']?></span><a href="#"><?php echo $suggestion['suggested']?></a><span class="tag-spacer left"><?php echo $suggestion['right']?></span></li>
 <?php endforeach ?>
 </ul>
