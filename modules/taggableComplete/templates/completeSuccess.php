900b958337222c10d395ab1040e67e22714f5e9f
John Benson <johnny@punkave.com>
2009-07-15 18:28:47 +0000

    added some styles to the tagAhead list
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineActAsTaggablePlugin/branches/punkave@20193 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/taggableComplete/templates/completeSuccess.php
+++ modules/taggableComplete/templates/completeSuccess.php
@@ -1,6 +1,6 @@
 <ul>
-<?php foreach($tagSuggestions as $suggestion): ?>
+<?php $n = 1; $limit = count($tagSuggestions); foreach($tagSuggestions as $suggestion): ?>
 <?php // No extraneous whitespace here, it shows up in text() ?>
-<li><span class="tag-spacer left"><?php echo $suggestion['left']?></span><a href="#"><?php echo $suggestion['suggested']?></a><span class="tag-spacer left"><?php echo $suggestion['right']?></span></li>
-<?php endforeach ?>
+<li><span class="tag-spacer left"><?php echo $suggestion['left']?></span><a <?php echo ($n == $limit)? 'class="last"':'' ?><?php echo ($n == 1)? 'class="first"':'' ?> href="#"><?php echo $suggestion['suggested']?></a><span class="tag-spacer left"><?php echo $suggestion['right']?></span></li>
+<?php $n++; endforeach ?>
 </ul>
